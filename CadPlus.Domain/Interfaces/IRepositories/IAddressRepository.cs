f35fdf71faf4695e85329c59bc53348a7d3a9ffb
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-09-23 19:57:04 -0300

    ✨ add removeUserAddresses and AddAddress features

--- CadPlus.Domain/Interfaces/IRepositories/IAddressRepository.cs
+++ CadPlus.Domain/Interfaces/IRepositories/IAddressRepository.cs
@@ -5,5 +5,8 @@ namespace CadPlus.Domain.Interfaces.IRepositories
     public interface IAddressRepository
     {
         Task<Address> CheckIfAddresAlreadyExists(string zipCode, string street);
+        Task RemoveUserAddresses(Guid userId, List<Guid> addressesExcluded);
+
+        Task AddAddress(Address address);
     }
 }
--- CadPlus.Infrastructure/Repositories/AddressRepository.cs
+++ CadPlus.Infrastructure/Repositories/AddressRepository.cs
@@ -18,5 +18,22 @@ namespace CadPlus.Infrastructure.Repositories
         {
             return await _context.Addresses.FirstOrDefaultAsync(a => a.ZipCode == zipCode && a.Street == street);
         }
+
+        public async Task RemoveUserAddresses(Guid userId, List<Guid> addressesExcluded)
+        {
+            var user = await _context.Users.Include(u => u.Addresses).FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user != null)
+            {
+                user.Addresses.RemoveAll(a => addressesExcluded.Contains(a.Id));
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task AddAddress(Address address)
+        {
+            await _context.Addresses.AddAsync(address);
+            await _context.SaveChangesAsync();
+        }
     }
 }
